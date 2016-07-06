16242ea4b834f243632b99581b13787041cce418
Matheus Ronfim <mronfim@gmail.com>
2016-07-06 16:53:45 -0400

    Implement basic profile structure

--- imports/ui/pages/profile/index.js
+++ imports/ui/pages/profile/index.js
@@ -1,4 +1,5 @@
 import { Template } from 'meteor/templating';
+import { Posts } from '/imports/api/posts/posts.js';
 import './profilePage.html';
 import './profilePage.less';
 
@@ -6,4 +7,19 @@ Template.profilePage.onCreated(function() {
   if (!this.data) {
     Router.go('homePage');
   }
+});
+
+Template.profilePage.helpers({
+  username() {
+    return Template.instance().data.profileUser.profile.username;
+  },
+  posts() {
+    return Posts.find(
+      {userId: this.profileUser._id},
+      {sort: {createdAt: -1},
+    }).fetch();
+  },
+  timeFromNow(date) {
+    return moment(date).fromNow();
+  },
 });
