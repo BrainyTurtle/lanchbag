6dde178ab187ac358ea8204ab005f438643200ff
Matheus Ronfim <mronfim@gmail.com>
2016-07-08 22:55:14 -0400

    Add masonry to user profile and style

--- imports/ui/layouts/main-layout/index.js
+++ imports/ui/layouts/main-layout/index.js
@@ -1,1 +1,2 @@
 import "./main-layout.html";
+import "./main-layout.less";
--- imports/ui/pages/profile/index.js
+++ imports/ui/pages/profile/index.js
@@ -12,6 +12,31 @@ Template.profilePage.onCreated(function() {
   });
 });
 
+Template.profilePage.onRendered(function() {
+  var resizeLayout = () => {
+    var profilePageWidth = $('.profile-page').width();
+    var asideWidth = $('.aside').outerWidth();
+    if ($(window).width() > 665) {
+      $('.wall').width(profilePageWidth - asideWidth);
+    } else {
+      $('.wall').width("100%");
+    }
+  }
+
+  resizeLayout();
+  $(window).resize(function() {
+    resizeLayout();
+  });
+
+  $('.posts-grid').isotope({
+    itemSelector: '.post',
+    masonry: {
+      columnWidth: '.post',
+      gutter: '.post-grid-gutter-sizer',
+    },
+  });
+});
+
 Template.profilePage.helpers({
   username() {
     return Template.instance().data.profileUser.profile.username;
