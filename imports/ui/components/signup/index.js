c0c2f9649ee10730a2fca102182279f9d54e8858
matheus <mronfim@gmail.com>
2016-07-10 16:23:36 -0400

    Added semantic-ui form validation on sigin and signup forms

--- imports/ui/components/signup/index.js
+++ imports/ui/components/signup/index.js
@@ -1,6 +1,105 @@
 import "./signup.html";
 import "./signup.less";
 
+Template.signup.onRendered(function() {
+  $('.ui.signup-form').form({
+    fields: {
+      firstName: {
+        identifier: 'firstName',
+        rules: [
+          {
+            type: 'empty',
+            prompt: 'Please enter your first name',
+          },
+          {
+            type: 'regExp[/^[A-Za-z]*$/]',
+            prompt: 'First name must contain only letters',
+          },
+          {
+            type: 'minLength[2]',
+            prompt: 'First name must be least 2 characters long',
+          },
+          {
+            type: 'maxLength[25]',
+            prompt: 'First name must be no more than 25 characters long',
+          },
+        ],
+      },
+      lastName: {
+        identifier: 'lastName',
+        rules: [
+          {
+            type: 'empty',
+            prompt: 'Please enter your last name',
+          },
+          {
+            type: 'regExp[/^[A-Za-z]*$/]',
+            prompt: 'Last name must contain only letters',
+          },
+          {
+            type: 'minLength[2]',
+            prompt: 'Last name must be least 2 characters long',
+          },
+          {
+            type: 'maxLength[25]',
+            prompt: 'Last name must be no more than 25 characters long',
+          },
+        ],
+      },
+      username: {
+        identifier: 'username',
+        rules: [
+          {
+            type: 'empty',
+            prompt: 'Please enter a username',
+          },
+          {
+            type: 'regExp[/^[A-Za-z][A-Za-z0-9_]*$/]',
+            prompt: 'Usernname must contain only alphanumeric characters',
+          },
+          {
+            type: 'minLength[3]',
+            prompt: 'Username must be least 2 characters long',
+          },
+          {
+            type: 'maxLength[15]',
+            prompt: 'Username must be no more than 15 characters long',
+          },
+        ],
+      },
+      email: {
+        identifier: 'email',
+        rules: [
+          {
+            type: 'email',
+            prompt: 'Please enter a valid email address',
+          },
+        ],
+      },
+      confirmPassword: {
+        identifier: 'confirmPassword',
+        rules: [
+          {
+            type: 'match[password]',
+            prompt: 'Does not match password',
+          },
+        ],
+      },
+      password: {
+        identifier: 'password',
+        rules: [
+          {
+            type: 'empty',
+            prompt: 'Please enter a password',
+          },
+        ],
+      },
+    },
+    inline: true,
+    on: 'blur',
+  });
+});
+
 Template.signup.events({
   'submit form.signup-form'(event, template) {
     event.preventDefault();
